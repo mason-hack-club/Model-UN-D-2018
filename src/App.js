57c7555c75c45d556bc838025f026b41bca5bc4f
itenpn <iten.pn@gmail.com>
2018-11-30 13:07:32 -0500

    Design Work

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Logo from  './.Assets/munlogo.png';
 import NavBar from './NavBar/NavBar.js';
-import Button from './Buttons/Button.js';
 
 class App extends React.Component{
 	render(){
@@ -10,7 +9,6 @@ class App extends React.Component{
 		<div>
 			<img className="logo" src={Logo} alt="Model UN"/>
 			<NavBar />
-			<Button />
 		</div>
 		);
 	}
--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -5,6 +5,13 @@ class NavBar extends React.Component{
 	render(){
 		return(
 		<div>
+		  <table>
+		    <tr>
+			<td className="head">ABOUT</td>
+			<td className="head">WMHSMUN</td>
+			<td className="head">GIL CAMP</td>
+		    </tr>
+		  </table>
 		</div>
 		);
 	}
